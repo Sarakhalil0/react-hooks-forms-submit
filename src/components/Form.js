3ec57feea2887da481cbb69d89e5e1702dc45b92
sara khalil <sarakhalil3377@gmail.com>
2022-10-12 16:32:39 +0300

    DONE

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 
-function Form(props) {
+function Form() {
   const [firstName, setFirstName] = useState("Sylvia");
   const [lastName, setLastName] = useState("Woods");
+  const [submittedData, setSubmittedData] = useState([])
+  const [errors, setErrors] = useState([])
 
   function handleFirstNameChange(event) {
     setFirstName(event.target.value);
@@ -12,12 +14,52 @@ function Form(props) {
     setLastName(event.target.value);
   }
 
+//
+function handleSubmit(event) {
+  event.preventDefault();
+  if(firstName.length > 0 && lastName.length > 0){
+  const formData = {
+    firstName: firstName,
+    lastName: lastName,
+  };
+  const dataArray = [...submittedData,formData]
+  setSubmittedData(dataArray )
+  setFirstName("");
+  setLastName("");
+  setErrors([])
+
+} else {
+  setErrors(["please enter name"])
+}
+
+}
+
+// submitted data display
+const listOfSubmissions = submittedData.map((data,index) => 
+<div key={index}> {data.firstName} {data.lastName} </div> ) 
+
+//error message display
+const errorMessage = errors.length > 0 //mabasty awaya agar error message ka lasare nusitman away naw (setErrors)
+  ? errors.map((error, index) => (       //gawratr bu la 0 binawa ,agar 100 dabneyn nayatawa error messageaka
+      <p key={index} style={{ color: "red" }}>
+        {error}
+      </p>
+    ))
+  : null
+
   return (
-    <form>
+    <div>
+    <form onSubmit={handleSubmit}>
       <input type="text" onChange={handleFirstNameChange} value={firstName} />
       <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
-    </form>
+      </form>
+
+      {errorMessage}
+
+    <h3>Submitted Data</h3>
+      {listOfSubmissions}
+      </div>
   );
 }
 
